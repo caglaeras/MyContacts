91ef72b47b6b1780ca3db0707fbdf65918714c0b
caglaeraslan@hotmail.com <caglaeraslan@hotmail.com>
2024-12-10 13:29:45 +0300

    Cagla Eraslan

--- MyContacts/Model/ContactsRepository.cs
+++ MyContacts/Model/ContactsRepository.cs
@@ -10,30 +10,34 @@ namespace MyContacts.Model
 {
     public class ContactsRepository
     {
-         static List<ContactInfo> contacts = new List<ContactInfo>() { 
-                    new ContactInfo {Id=1, NameSurname="Hüseyin Şimşek", Email="huseyinsimsek@gmail", PhoneNumber="053357252"}
-                    };
-        static int maxId = 2;
-        public ContactsRepository()
-        {
-            
-        }
-
-        public ObservableCollection<ContactInfo> GetContacts() {
-            return new ObservableCollection<ContactInfo>( contacts);
-
-        }
+        private List<ContactInfo> contacts = new List<ContactInfo>();
 
         public void AddContact(ContactInfo contact)
         {
-            contact.Id = maxId++;
+            contact.Id = contacts.Count > 0 ? contacts.Max(c => c.Id) + 1 : 1; // Auto-increment ID
             contacts.Add(contact);
         }
 
         public ContactInfo GetContact(int id)
         {
-            return contacts.FirstOrDefault( c => c.Id==id ) ;
+            return contacts.FirstOrDefault(c => c.Id == id);
         }
 
+        public List<ContactInfo> GetContacts()
+        {
+            return contacts;
+        }
+
+        public void UpdateContact(ContactInfo updatedContact)
+        {
+            var contact = GetContact(updatedContact.Id);
+            if (contact != null)
+            {
+                contact.NameSurname = updatedContact.NameSurname;
+                contact.PhoneNumber = updatedContact.PhoneNumber;
+                contact.Email = updatedContact.Email;
+            }
+        }
     }
+
 }
--- MyContacts/Views/EditContactPage.xaml.cs
+++ MyContacts/Views/EditContactPage.xaml.cs
@@ -2,24 +2,55 @@ using MyContacts.Model;
 
 namespace MyContacts.Views;
 
-
-
-[QueryProperty(nameof(id), "id")]
+[QueryProperty(nameof(Id), "id")]
 public partial class EditContactPage : ContentPage
 {
     ContactsRepository repository = new ContactsRepository();
 
-    public string id { get; set; }
-	public EditContactPage()
-	{
-        
-		InitializeComponent();
-	}
+    public string Id { get; set; }
+    private ContactInfo contact;
+
+    public EditContactPage()
+    {
+        InitializeComponent();
+    }
+
     protected override void OnAppearing()
     {
         base.OnAppearing();
-        
-        var contact = repository.GetContact(Int32.Parse(id));
-        selectedContact.Text = id + " " + contact.NameSurname;
+
+        if (!string.IsNullOrEmpty(Id))
+        {
+            contact = repository.GetContact(Int32.Parse(Id));
+            if (contact != null)
+            {
+                NameEntry.Text = contact.NameSurname;
+                PhoneEntry.Text = contact.PhoneNumber;
+                EmailEntry.Text = contact.Email;
+            }
+        }
+    }
+
+    private void SaveButton_Clicked(object sender, EventArgs e)
+    {
+        if (contact != null)
+        {
+            // Update contact details
+            contact.NameSurname = NameEntry.Text;
+            contact.PhoneNumber = PhoneEntry.Text;
+            contact.Email = EmailEntry.Text;
+
+            // Save updated contact
+            repository.UpdateContact(contact);
+
+            // Navigate back
+            Shell.Current.GoToAsync("..");
+        }
+    }
+
+    private void CancelButton_Clicked(object sender, EventArgs e)
+    {
+        // Navigate back without saving changes
+        Shell.Current.GoToAsync("..");
     }
 }
